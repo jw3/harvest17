22a5ba2f2c636131c39a14c59c52f486eb039be6
John Wass <jwass3@gmail.com>
2017-10-02 06:53:06 -0400

    disable predictable net names
    resolves #8

--- setup/install.sh
+++ setup/install.sh
@@ -19,6 +19,12 @@ cp ${rootdir}/etc/default/* /etc/default
 cat ${rootdir}/etc/hosts.append.${profile} | tee -a /etc/hosts
 
 #
+# disable predictable network names
+#
+ln -s /dev/null /etc/systemd/network/99-default.link
+
+
+#
 # update rc.d
 #
 update-rc.d isc-dhcp-server start
